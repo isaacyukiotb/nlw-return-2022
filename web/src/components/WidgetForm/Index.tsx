e2987da9cee4ad28b625f7e6fb5f44e48b81331b
Isaac Yukio T. Barros <isaacyukio@gmail.com>
2022-05-05 19:02:35 -0300

    Melhorando o Formulário WidgetForm

---
+++ web/src/components/WidgetForm/Index.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+
+import { FeedbackContentStep } from "./steps/FeedbackContentStep";
+import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
+
+
+
+export const feedbackTypes = {
+    BUG: {
+        title:'Problema',
+        image: {
+            source: bugImageUrl,
+            alt:'Imagem de um inseto'
+        }
+    },
+    IDEA: {
+        title:'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt:'Imagem de uma lâmpada'
+        }
+    },
+    THOUGHT: {
+        title:'Outros',
+        image: {
+            source: thoughtImageUrl,
+            alt:'Imagem de um balão de ideia'
+        }
+    }
+}
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+
+    const [feedBackType, setFeedBackType] = useState<FeedbackType | null>(null);
+
+    function handleFeedbackRestart(){
+        setFeedBackType(null);
+    }
+
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2em)] md:w-auto">
+
+            
+            {!feedBackType ?
+                (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedBackType}/>
+                )
+                :
+                <FeedbackContentStep onFeedbackRestartRequested={handleFeedbackRestart} feedbackType={feedBackType}/>
+            }
+
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥︎ by <a target={"_blank"} href="https://github.com/isaacyukiotb" className="underline underline-offset-2">Isaac Yukio</a>
+            </footer>
+
+        </div>
+    );
+}
--- web/src/components/WidgetForm/steps/FeedbackTypeStep.tsx
+++ web/src/components/WidgetForm/steps/FeedbackTypeStep.tsx
@@ -1,26 +1,34 @@
-import { feedBackType, feedbackTypes } from "../../WidgetForm"
+import { CloseButton } from "../../CloseButton";
+import { FeedbackType, feedbackTypes } from "../Index"
 
-interface FeedBackStepProps{
-    onFeedbackTypeChanged:(type:feedBackType) => void;
+interface FeedBackStepProps {
+    onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 
-export function FeedbackTypeStep(props:FeedBackStepProps){
-    return(
-        <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, values]) => {
-            return (
-                <button
-                    key={key}
-                    className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                    onClick={() => props.onFeedbackTypeChanged(key as feedBackType)}
-                    type="button"
-                >
-                    <img src={values.image.source} alt={values.image.alt} />
-                    <span>{values.title}</span>
-                </button>
-            )
-        })
-        }
-    </div>
+export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedBackStepProps) {
+    return (
+        <>
+            <header>
+                <span className="text-xl leading-6">Deixe seu FeedBack</span>
+                <CloseButton />
+            </header >
+            
+            <div className="flex py-8 gap-2 w-full">
+                {Object.entries(feedbackTypes).map(([key, values]) => {
+                    return (
+                        <button
+                            key={key}
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+                            type="button"
+                        >
+                            <img src={values.image.source} alt={values.image.alt} />
+                            <span>{values.title}</span>
+                        </button>
+                    )
+                })
+                }
+            </div>
+        </>
     )
 }
