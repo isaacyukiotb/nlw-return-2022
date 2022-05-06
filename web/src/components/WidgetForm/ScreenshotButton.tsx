bbe20b3286f18ff890a75cbfe8c12df98a2a89eb
Isaac Yukio T. Barros <isaacyukio@gmail.com>
2022-05-06 20:07:03 -0300

    Finalizando o botao de Screen

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,32 +1,57 @@
 import html2canvas from "html2canvas";
-import { Camera } from "phosphor-react";
+import { backgroundPosition } from "html2canvas/dist/types/css/property-descriptors/background-position";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
 import { Loading } from "../Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+    screenshot: string | null;
+    onScreenshotTook: (image: string | null) => void;
+}
+
+export function ScreenshotButton(props: ScreenshotButtonProps) {
 
     const [isTakeScreenshot, setIsTakeScreenshot] = useState(false)
 
-    async function handleTakeScreenshot(){
-        
+    async function handleTakeScreenshot() {
+
         setIsTakeScreenshot(true);
 
-       const canvas = await html2canvas(document.querySelector('html')!);
-       const base64image = canvas.toDataURL('image/png');
-       
-       console.log(base64image);
+        const canvas = await html2canvas(document.querySelector('html')!);
+        const base64image = canvas.toDataURL('image/png');
 
-       //setIsTakeScreenshot(false);
+        props.onScreenshotTook(base64image);
+
+        setIsTakeScreenshot(false);
+    }
+
+    if (props.screenshot) {
+        return (
+            <button
+                type="button"
+                className="p-1 w-10 h-10 rounded-md border-transparent flex items-end justify-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                onClick={() => props.onScreenshotTook(null)}
+                style={{
+                    backgroundImage:`URL(${props.screenshot})`,
+                }} 
+            >
+                <Trash />
+
+            </button>
+        )
     }
 
     return (
-        
+
         <button
             type="button"
             onClick={handleTakeScreenshot}
             className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
         >
-            {isTakeScreenshot ? <Loading/> : <Camera className="text-zinc-100 w-6 h-6" />}
+            {isTakeScreenshot ? <Loading /> : <Camera className="text-zinc-100 w-6 h-6" />}
         </button>
+
+
+
     )
 }
--- web/src/components/WidgetForm/steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/steps/FeedbackContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { CloseButton } from "../../CloseButton";
 import { FeedbackType, feedbackTypes } from "../Index";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -8,9 +9,14 @@ interface FeedbackContentStepProps {
     onFeedbackRestartRequested: () => void;
 }
 
+
 export function FeedbackContentStep(props: FeedbackContentStepProps) {
 
+
+    const [screenshot, setScreenshot] = useState<string | null>(null)
+
     const feedbackTypeInfo = feedbackTypes[props.feedbackType];
+
 
     return (
         <>
@@ -31,24 +37,27 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
             </header >
 
             <form className="my-4 w-full ">
-                <textarea 
-                className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-                placeholder="Conte com detalhes o que está acontecendo..."
+                <textarea
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que está acontecendo..."
                 />
 
                 <footer className="flex gap-2 mt-2">
-                    
-                    <ScreenshotButton/>
+
+                    <ScreenshotButton
+                        onScreenshotTook={setScreenshot}
+                        screenshot={screenshot}
+                    />
 
                     <button
-                    type="submit"
-                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
                     >
                         Enviar
                     </button>
                 </footer>
             </form>
-            
+
         </>
     )
 }
