310b76b40dd2bbde2bb076dff93b3981c031a1fb
Isaac Yukio T. Barros <isaacyukio@gmail.com>
2022-05-07 10:59:54 -0300

    Criando, configurando e instalando as dependencias  do server (Backend) da aplicacao

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/users', (req,res) =>{
+    res.send('Hello world!');
+})
+
+app.listen('3333',() =>{
+    console.log('HTTP is running!');
+})
